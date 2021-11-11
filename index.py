7bc6f2073a2ec981af5dd016341084ee182ad40a
Raul Luca <raulgalatafc@gmail.com>
2021-11-11 17:25:32 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
-import time, os, ftplib
+import time, os, ftplib, nmap, warnings, pysftp
+from cryptography.utils import DeprecatedIn25
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-pathDownload = r"C:\Users\Asus\Documents\Cisco\projetos\Python\2\temp\\"
+pathDownload = r"/home/e3/Report//"
 
 prefs = {"profile.default_content_settings.popups": 0,
         "download.default_directory": pathDownload,
@@ -12,23 +13,26 @@ prefs = {"profile.default_content_settings.popups": 0,
 
 options = Options()
 options.add_experimental_option('prefs', prefs)
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-driver= webdriver.Chrome(options=options, service_log_path='NUL',executable_path=r'./chromedriver.exe')
+driver= webdriver.Chrome(options=options,executable_path=r'./chromedriver')
 driver.get('https://dashboards.flowbix.com/grafana/login')
 
+
 # Section: Login
-# 
+#
 # Collect login input via XPATH.
-sendUsername = driver.find_element(By.XPATH, '/html/body/grafana-app/div/div/react-container/div/div/div[2]/div/div/form/div[1]/div[2]/div/div/input')
-sendUsername.send_keys('cliente.licknet')
+sendUsername = driver.find_element(By.XPATH, '/html/body/grafana-app/div/div/react-container/div/div/div[2]/div/div/form/div[1]/div[2]/div/div/input').send_keys('cliente.licknet')
+
 # Collect password input via XPATH.
-sendPassword = driver.find_element(By.XPATH, '/html/body/grafana-app/div/div/react-container/div/div/div[2]/div/div/form/div[2]/div[2]/div/div/input')
-sendPassword.send_keys('cliente.licknet1728')
+sendPassword = driver.find_element(By.XPATH, '/html/body/grafana-app/div/div/react-container/div/div/div[2]/div/div/form/div[2]/div[2]/div/div/input').send_keys('cliente.licknet1728')
+
 # Collect button submit via XPATH.
 sendSubmit = driver.find_element(By.XPATH, '/html/body/grafana-app/div/div/react-container/div/div/div[2]/div/div/form/button').click()
+
 
 # Section: Entering in board
 #
@@ -37,7 +41,7 @@ time.sleep(10)
 clickBoard = driver.find_element(By.XPATH, '/html/body/grafana-app/div/div/react-container/div/div[2]/div/div[1]/div/div/div[2]/div/div[1]/div/div[2]/div/plugin-component/panel-plugin-dashlist/grafana-panel/ng-transclude/div/div[1]/div/div[2]/a/div/div').click()
 
 #
-# 
+#
 time.sleep(10)
 clickLabel = driver.find_element(By.XPATH, '/html/body/grafana-app/div/div/react-container/div/div[2]/div/div[1]/div/div[2]/div[12]/div/div[1]/div/div[1]').click()
 
@@ -58,27 +62,42 @@ clickDownloadFile = driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div
 
 time.sleep(2)
 
-ftp = ftplib.FTP()
-host = "192.168.2.8"
-port = 21
-ftp.connect(host, port)
-
 try:
-    if ftp.login("tin", "123"):
-        for item in os.listdir(pathDownload):
-            fileRenamed = 'doc_relatorio_sao-mateus.csv'
+	ftp = ftplib.FTP()
+	host = "192.168.2.2"
+	user = "root"
+	passw= "samba#2020!!"
 
-            os.rename(rf'.\temp\{item}', rf'.\temp\{fileRenamed}')
+	warnings.simplefilter('ignore', DeprecatedIn25)
+	checkPort = nmap.PortScanner()
+	checkPort.scan(host, '21-22')
 
-            try:
-                uploadFile = open(rf'.\temp\{fileRenamed}','rb')
-                ftp.storbinary(rf'STOR {fileRenamed}', uploadFile)
-                uploadFile.close()
-                time.sleep(1)
-                os.remove(r'C:\Users\Asus\Documents\Cisco\projetos\Python\2\temp\\' + fileRenamed)
-                
-            except Exception as errorUpload:
-                print(errorUpload)
+	try:
+		if(checkPort[host]['tcp'][22]['state'] == "closed"):
+			ftp.connect(host, 21)
+			if ftp.login(user, passw):
+				for item in os.listdir(pathDownload):
+					fileRenamed = 'doc_relatorio_sao-mateus.csv'
+					os.rename(rf'/home/e3/Report/{item}', rf'/home/e3/Report//{fileRenamed}')
+
+				try:
+					uploadFile = open(rf'/home/e3/Report/{fileRenamed}','rb')
+					ftp.storbinary(rf'STOR {fileRenamed}', uploadFile)
+					uploadFile.close()
+					time.sleep(1)
+    	            #os.remove(r'/home/e3/SeleniumAutoReport/Report/temp//' + fileRenamed)
+
+				except Exception as errorUpload:
+					print(errorUpload)
+		elif(checkPort[host]['tcp'][21]['state'] == "closed"):
+			cnopts = pysftp.CnOpts()
+			cnopts.hostkeys = None
+			with pysftp.Connection(host, username=user, password = passw, cnopts=cnopts) as sftp:
+				sftp.cwd(r"/home/arquivos_licknet_2019/geral/NOC/Relátorios/Analises")
+				sftp.put(r"/home/e3/Report/doc_relatorio_sao-mateus.csv")
+				sftp.close()
+	except Exception as ErroFTP:
+		print(ErroFTP)
 
 except Exception as e:
     print (e)
